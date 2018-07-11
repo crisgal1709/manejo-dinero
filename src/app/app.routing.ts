f8060bb0a888a31178bb7ccc8a0812fdbb821f27
Desarrollador <cristian.galeano1913@gmail.com>
2018-07-11 06:54:02 -0500

    Add Router-outlet

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,5 +1,5 @@
 import { ModuleWithProviders } from '@angular/core';
-import { Routes,RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 
 //Carga los componentes
